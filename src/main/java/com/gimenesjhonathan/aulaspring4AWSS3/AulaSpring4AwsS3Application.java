1c1507f6e8347420d87e1a02ed87b07fcdc98ff8
Jhonathan Gimenes <gimenesjhonathan@gmail.com>
2021-02-13 20:10:15 -0300

    Primeiro commit

---
+++ src/main/java/com/gimenesjhonathan/aulaspring4AWSS3/AulaSpring4AwsS3Application.java
@@ -0,0 +1,13 @@
+package com.gimenesjhonathan.aulaspring4AWSS3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpring4AwsS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpring4AwsS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimenesjhonathan/aulaspring4AWSS3/AulaSpring4AwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimenesjhonathan.aulaspring4AWSS3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpring4AwsS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
